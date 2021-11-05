bf566116bbddfdd2feffebc94a4730d3f5d67df7
fengbo <fengbo.lucky@bytedance.com>
2021-11-05 10:35:01 +0800

    feat(events): add empty func

--- packages/mona-plugin-events/src/index.ts
+++ packages/mona-plugin-events/src/index.ts
@@ -1,15 +1,29 @@
-import { MonaPluginEvents } from './type';
+import { ErrorCode, InputCallbackParams, MonaPluginEvents } from './type';
 
 const SDK_NAME_IN_WINDOW = '____MONA_SDK_NAME_IN_WINDOW____';
-// !只能在内部sdk出现，外部需删去
+
 const MONA_GLOBAL_STORE = 'monaGlobalStore';
+
+const emptyHandler = {
+  get: function (_: any, prop: string) {
+    return (data: InputCallbackParams) => {
+      console.info(`非飞鸽容器, ${prop} 方法无法调用`);
+      if (typeof data === 'object') {
+        data?.failed?.({ code: ErrorCode.Unknown, message: '非飞鸽容器' });
+      }
+    };
+  },
+  set: () => false
+};
+const emptyObj = new Proxy({}, emptyHandler);
+
 
 const handler = {
   get: function (_: any, prop: string) {
     if (prop === 'globalStore') {
-      return window?.[MONA_GLOBAL_STORE].pigeon.xxxx;
+      return window?.[MONA_GLOBAL_STORE];
     }
-    return window?.[SDK_NAME_IN_WINDOW]?.[prop];
+    return window?.[SDK_NAME_IN_WINDOW]?.[prop] || emptyObj;
   },
   set: () => false
 };
--- packages/mona-plugin-events/src/pigeon.type.ts
+++ packages/mona-plugin-events/src/pigeon.type.ts
@@ -3,6 +3,14 @@ import { InputCallbackParams, PluginSdkBaseType } from './type';
 type OfficialParams = 'after_sale_id' | 'order_id' | 'product_id' | 'sku_order_id';
 
 export interface lifeCycleShow {
+  /**
+   * 0：点击tab进入
+   * 1~4 自定义按钮跳转
+   * 1：店铺单进入 order_id
+   * 2：商品单进入 sku_order_id
+   * 3：售后单进入 after_sale_id
+   * 4：商品列表进入 product_id
+   */
   showFrom: 0 | 1 | 2 | 3 | 4;
   query: Record<OfficialParams, string>;
   extraData: Record<string, string>;
