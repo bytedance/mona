99fe48ad5ad9a3eeeaae723809d0132969a52c46
xwchris <zhangxiaowei.chris@bytedance.com>
2021-11-11 17:47:37 +0800

    chore: build mona-plugin-events

--- packages/mona-plugin-events/dist/pigeon.type.d.ts
+++ packages/mona-plugin-events/dist/pigeon.type.d.ts
@@ -1,8 +1,24 @@
 import { InputCallbackParams, PluginSdkBaseType } from './type';
 declare type OfficialParams = 'after_sale_id' | 'order_id' | 'product_id' | 'sku_order_id';
+/**
+ * 打开插件时，插件生命周期onShow接受的参数showFrom
+ * 0：点击tab进入
+ * 1~4 自定义按钮跳转
+ * 1：店铺单进入 order_id
+ * 2：商品单进入 sku_order_id
+ * 3：售后单进入 after_sale_id
+ * 4：商品列表进入 product_id
+ */
+export declare enum PluginShowFrom {
+    clickTab = 0,
+    aftersaleOrder = 3,
+    skuOrder = 2,
+    shopOrder = 1,
+    product = 4
+}
 export interface lifeCycleShow {
-    showFrom: 0 | 1 | 2 | 3 | 4;
-    query: Record<OfficialParams, string>;
+    showFrom: PluginShowFrom;
+    query: Partial<Record<OfficialParams, string>>;
     extraData: Record<string, string>;
 }
 export interface UserInfo {
