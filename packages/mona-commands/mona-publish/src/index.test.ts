59c14691925f5def65bbca81ac3277a30fe17456
fengbo <fengbo.lucky@bytedance.com>
2021-11-23 17:10:22 +0800

    chore: test actions

--- packages/mona-commands/mona-publish/src/index.test.ts
+++ packages/mona-commands/mona-publish/src/index.test.ts
@@ -6,7 +6,7 @@ import path from 'path';
 test('pkg start', async () => {
   expect((await compressToZipFromDir(__dirname))?.length > 0).toBe(true);
 
-  fse.rm(path.resolve(__dirname, '..', 'publish.zip'));
+  await fse.remove(path.resolve(__dirname, '../', 'publish.zip'));
 });
 
 test('commandUsage', async () => {
