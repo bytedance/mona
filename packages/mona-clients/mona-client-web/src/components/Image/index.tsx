f33c5a4952606a0b717f6a1847d1bae2a9878c77
xwchris <zhangxiaowei.chris@bytedance.com>
2021-12-09 17:15:31 +0800

    chore: finish scroll view

--- packages/mona-clients/mona-client-web/src/components/Image/index.tsx
+++ packages/mona-clients/mona-client-web/src/components/Image/index.tsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { ImageProps } from '@bytedance/mona';
 import cs from 'classnames';
 import styles from './index.module.less';
 import { useHandlers } from '../hooks';
 import { formatSyntheticEvent } from '../utils';
 
-const ImageComponent: React.FC<ImageProps> = ({ children, src, mode = 'scaleToFill', lazyLoad, onError, onLoad, ...restProps }) => {
-  const { handleClassName, ...handlerProps} = useHandlers(restProps)
+const ImageComponent: React.FC<ImageProps> = ({ children, src, mode = 'scaleToFill', lazyLoad = false, onError, onLoad, ...restProps }) => {
+  const { handleClassName, ...handlerProps} = useHandlers(restProps);
   const [url, setUrl] = useState('');
+  const [load, setLoad] = useState(!lazyLoad);
+  const ref = useRef<HTMLDivElement | null>(null);
 
   const handleLoad = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
     setUrl(url);
@@ -21,11 +23,23 @@ const ImageComponent: React.FC<ImageProps> = ({ children, src, mode = 'scaleToFi
       onError(formatSyntheticEvent({ event: e }));
     }
   }
+
+  const onScroll = (scrollEle: Element) => {
+    const scrollTop = scrollEle.scrollTop;
+    const clientHeight = scrollEle.clientHeight;
+    const rect = ref.current?.getBoundingClientRect();
+    const top = rect?.top || 0;
+    if ((scrollTop + clientHeight) > top) {
+      setLoad(true);
+    }
+  }
+
+  console.log('onScroll', onScroll);
   
   return (
-    <div {...handlerProps} className={handleClassName(styles.wrapper)}>
-      <img className={styles.hidden} src={src} onLoad={handleLoad} onError={handleError} />
-      <div style={{ backgroundImage: `url("${url}")` }} className={cs(styles.image, styles[mode.replace(/\s/, '')])}></div>
+    <div ref={ref} {...handlerProps} className={handleClassName(styles.wrapper)}>
+      {load ? <img className={styles.hidden} src={src} onLoad={handleLoad} onError={handleError} /> : null}
+      {url ? <div style={{ backgroundImage: `url("${url}")` }} className={cs(styles.image, styles[mode.replace(/\s/, '')])}></div> : url}
     </div>
   )
 }
