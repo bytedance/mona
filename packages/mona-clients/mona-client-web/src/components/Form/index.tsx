778411997647766c0cc5ebe053d4ac1265f62ce9
xwchris <zhangxiaowei.chris@bytedance.com>
2021-12-14 21:17:24 +0800

    chore: wip input

--- packages/mona-clients/mona-client-web/src/components/Form/index.tsx
+++ packages/mona-clients/mona-client-web/src/components/Form/index.tsx
@@ -1,8 +1,73 @@
-import React from 'react';
+import React, { createContext, useRef } from 'react';
 import { FormProps } from '@bytedance/mona';
+import { useHandlers } from '../hooks';
 
-const Form: React.FC<FormProps> = ({ children }) => {
-  return <div>{children}</div>
+interface FormContextProps {
+  register: (submit: () => { name: string; value: any }, reset: () => void, order?: number | null) => number;
+  unregister: (registerId: number | null) => void;
+}
+export const FormContext = createContext<FormContextProps | null>(null);
+
+const Form: React.FC<FormProps> = (props) => {
+  const {
+    children,
+    onSubmit,
+    onReset,
+    ...restProps
+  } = props;
+
+  const { handleClassName, ...handlerProps} = useHandlers(restProps)
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    const cbs = cbsRef.current;
+    const result: Record<string, any> = {};
+    cbs.forEach(cb => {
+      const { submit } = cb;
+      const { name, value } = submit();
+      if (name) {
+        result[name] = value;
+      }
+    })
+
+    const event = {
+      value: result
+    }
+    console.log(event);
+  }
+
+  const handleReset = () => {
+    const cbs = cbsRef.current;
+    cbs.forEach(cb => {
+      const { reset } = cb;
+     reset();
+    })
+  }
+
+  const indexRef = useRef(0);
+  const cbsRef = useRef<{submit: () => { name: string; value: any }, reset: () => void}[]>([]);
+  const context: FormContextProps = {
+    register: (submit, reset, order) => {
+      const index = order == null ? indexRef.current++ : order;
+      cbsRef.current[index] = { submit, reset };
+      return index;
+    },
+    unregister: (index) => {
+      if (index != null) {
+        cbsRef.current.splice(index, 1);
+      }
+    }
+  }
+
+  
+  return (
+    <form onSubmit={handleSubmit} onReset={handleReset} className={handleClassName()} {...handlerProps}>
+      <FormContext.Provider value={context}>
+        {children}
+      </FormContext.Provider>
+    </form>
+  )
 }
 
 export default Form;
--- packages/mona-clients/mona-client-web/src/components/Input/index.tsx
+++ packages/mona-clients/mona-client-web/src/components/Input/index.tsx
@@ -1,12 +1,63 @@
-import React from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { InputProps } from '@bytedance/mona';
 import styles from './index.module.less';
+import { FormContext } from '../Form';
 import { useHandlers } from '../hooks';
 
-const Input: React.FC<InputProps> = ({ children, ...restProps }) => {
+const Input: React.FC<InputProps> = (props) => {
+  const {
+    children,
+    name = '',
+    value: initValue = '',
+    type = 'text',
+    password = false,
+    placeholderStyle,
+    maxLength = 140,
+    // TODO
+    focus = false,
+    cursorSpacing = 0,
+    cursor = -1,
+    selectionStart = -1,
+    selectionEnd = -1,
+    style = {},
+    onInput,
+    onFocus,
+    onBlur,
+    onConfirm,
+    adjustPosition = true,
+    confirmType = 'done',
+    ...restProps
+  } = props;
+
   const { handleClassName, ...handlerProps} = useHandlers(restProps)
+  const context = useContext(FormContext);
+  const [value, setValue] = useState(initValue);
+  const orderRef = useRef<number | null>(null);
+
+  useEffect(() => {
+    if (context) {
+      const index = orderRef.current;
+      orderRef.current = context.register(
+        () => ({ name, value }),
+        () => setValue(''),
+        index
+      )
+
+      return () => {
+        context.unregister(index);
+      }
+    }
+    return () => {}
+  }, [context, name, value])
+
+  const handleInput: React.FormEventHandler<HTMLInputElement> = (e) => {
+    const newValue = (e.target as HTMLInputElement).value;
+    setValue(newValue);
+  }
+
+  const inputType = password ? type === 'text' ? 'text' : 'number' : 'password';
   
-  return <input className={handleClassName(styles.input)} {...handlerProps} />
+  return <input maxLength={maxLength} name={name} value={value} type={inputType} onInput={handleInput} className={handleClassName(styles.input)} {...handlerProps} />
 }
 
 export default Input;
