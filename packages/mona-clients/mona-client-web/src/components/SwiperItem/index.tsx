57a48a5b695b4e8ce2fe5cf8503839324b9f6ae8
xwchris <zhangxiaowei.chris@bytedance.com>
2021-12-14 17:14:03 +0800

    chore: finish component slider

--- packages/mona-clients/mona-client-web/src/components/SwiperItem/index.tsx
+++ packages/mona-clients/mona-client-web/src/components/SwiperItem/index.tsx
@@ -4,8 +4,6 @@ import styles from './index.module.less';
 import { useHandlers } from '../hooks';
 
 const SwiperItem: React.FC<SwiperItemProps> = ({ children, itemId, ...restProps }) => {
-  console.log(restProps);
-
   const { handleClassName, ...handlerProps} = useHandlers(restProps);
 
   return <div className={handleClassName(styles.item)} {...handlerProps}>{children}</div>;
