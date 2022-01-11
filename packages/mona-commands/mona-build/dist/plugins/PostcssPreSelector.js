d815d3a5cd7f10aa83fe9bbfb56dbf9fe7eac931
xwchris <zhangxiaowei.chris@bytedance.com>
2022-01-11 19:15:48 +0800

    fix: fix postcss style

--- packages/mona-commands/mona-build/dist/plugins/PostcssPreSelector.js
+++ packages/mona-commands/mona-build/dist/plugins/PostcssPreSelector.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const processed = Symbol('processed');
-const reg = /(?:^|[ ]|,)(html|body)(?:$|[ ]|,)/;
+const reg = /(?:^|\s|,)(html|body)(?:$|\s|,)/;
+const htmlReg = /(?:^|\s|,)(html)(?:$|\s|,)/;
 module.exports = (opts) => {
     return {
         postcssPlugin: 'postcss-pre-selector',
@@ -9,15 +10,14 @@ module.exports = (opts) => {
             var _a;
             if (!rule[processed] && ((_a = rule === null || rule === void 0 ? void 0 : rule.parent) === null || _a === void 0 ? void 0 : _a.type) !== 'atrule') {
                 const s = (opts === null || opts === void 0 ? void 0 : opts.selector) || '';
-                rule.selector = `${s ? `${s} ` : ''}${rule.selector}`;
+                const newSelector = rule.selector.split(',').map(i => !htmlReg.test(i) ? `${s ? `${s} ` : ''}${i}` : s).join(',');
+                // add prefix to all rule except html
+                rule.selector = newSelector;
                 rule[processed] = true;
                 // replace html and body
                 if (reg.test(rule.selector)) {
-                    let selector = rule.selector;
-                    const replace = (a) => a.replace(/(?:^|[ ]|,)(html|body)(?:$|[ ]|,)/, (s, s1) => s.replace(s1, `div[__marfishmock${s1}__]`));
-                    while (reg.test(selector)) {
-                        selector = replace(selector);
-                    }
+                    const replace = (a) => a.replace(reg, (s, s1) => s.replace(s1, `div[__marfishmock${s1}__]`));
+                    const selector = replace(rule.selector);
                     const newRule = rule.cloneAfter({ selector });
                     newRule[processed] = true;
                 }
