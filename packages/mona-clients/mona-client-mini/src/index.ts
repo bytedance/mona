a934a66851c444447fdd0fd3ff9f380d0aa4d787
xwchris <zhangxiaowei.chris@bytedance.com>
2022-01-14 18:44:43 +0800

    chore: change name of runtime file

--- packages/mona-clients/mona-client-mini/src/index.ts
+++ packages/mona-clients/mona-client-mini/src/index.ts
@@ -1,7 +1,5 @@
 export * from './apis';
 export * from './components';
-// export * from './components/prop';
 export { default as createApp } from './createApp';
 export { default as createPage } from './createPage';
 export { default as createMiniComponent } from './createMiniComponent';
-export { usePageEvent, useAppEvent, AppLifecycle, PageLifecycle } from '@bytedance/mona';
