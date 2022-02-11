cb9eefc70b6e21bfa4bceca4e54d2ad3d2f9b193
fengbo <fengbo.lucky@bytedance.com>
2022-02-11 14:53:43 +0800

    chore(mona): createConfig jest

--- packages/mona/src/__tests__/index.test.ts
+++ packages/mona/src/__tests__/index.test.ts
@@ -2,12 +2,35 @@ import { createProjectConfig, createAppConfig, createPageConfig } from '../index
 
 describe('mona createConfig', () => {
   test('createProjectConfig', async () => {
-    expect(() => createProjectConfig({})).not.toThrow();
+    expect(() =>
+      createProjectConfig({
+        projectName: 'mona',
+        input: './src/app.tsx',
+        output: 'dist',
+      }),
+    ).not.toThrow();
+    expect(
+      createProjectConfig({
+        projectName: 'mona',
+        input: './src/app.tsx',
+        output: 'dist',
+      }),
+    ).toMatchSnapshot();
   });
   test('createProjectConfig', async () => {
-    expect(() => createAppConfig({})).not.toThrow();
+    expect(() =>
+      createAppConfig({
+        pages: [],
+      }),
+    ).not.toThrow();
+    expect(
+      createAppConfig({
+        pages: [],
+      }),
+    ).toMatchSnapshot();
   });
   test('createProjectConfig', async () => {
     expect(() => createPageConfig({})).not.toThrow();
+    expect(createPageConfig({})).toMatchSnapshot();
   });
 });
