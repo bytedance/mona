9ff059d8fd6af9017f35eebf38bc6fabb7bd71e7
fengbo <fengbo.lucky@bytedance.com>
2022-03-07 16:52:33 +0800

    fix: lifeCycle set forEach

--- packages/mona-clients/mona-client-mini/src/createPage.tsx
+++ packages/mona-clients/mona-client-mini/src/createPage.tsx
@@ -134,7 +134,8 @@ function createConfig(PageComponent: React.ComponentType<any>) {
 
     $callLifecycle(name: PageLifecycle, ...params: any[]) {
       const cbs = this._pageLifecycleContext.lifecycle[name] || new Set([]);
-      cbs.forEach(cb => batchedUpdates(params => cb(...params), params));
+      // Array.from 转数组，避免cb更改cbs，导致动态遍历
+      Array.from(cbs).forEach(cb => batchedUpdates(params => cb(...params), params));
       if (pageEntryRef.current?.[name]) {
         return pageEntryRef.current[name](...params);
       }
