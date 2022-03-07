2275aeff57761320c0b3405050b052a0e23c5509
fengbo <fengbo.lucky@bytedance.com>
2022-03-07 19:56:16 +0800

    feat(runtime): useMiniEffect

--- packages/mona-clients/mona-client-mini/src/index.ts
+++ packages/mona-clients/mona-client-mini/src/index.ts
@@ -3,3 +3,4 @@ export { default as createApp } from './createApp';
 export { default as createPage } from './createPage';
 export { default as createMiniComponent } from './createMiniComponent';
 export { Mona } from './apis';
+export { useMiniEffect } from './miniEffect';
