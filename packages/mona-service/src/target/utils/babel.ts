73d1175f12dc1026567321637cf1dcc23366a698
fengbo <fengbo.lucky@bytedance.com>
2022-03-24 16:55:28 +0800

    fix: babel

--- packages/mona-service/src/target/utils/babel.ts
+++ packages/mona-service/src/target/utils/babel.ts
@@ -1,13 +1,23 @@
+import { NodePath } from '@babel/core';
 import * as t from '@babel/types';
 import { isReactCreateElement } from './utils';
 
 // TODO:收集别名, 兼容react17不需要引入React即可直接书写jsx产生的问题
 // const jsxAlias = new Set(['jsx', '_jsx', 'jsxs', '_jsxs', 'jsxDEV']);
 
-export const isReactCall = (memberExpression: t.CallExpression['callee']) => {
+export function getImportName(path: NodePath<t.CallExpression>, name: string) {
+  // @ts-ignore
+  return path.scope.getBinding(name)?.path?.parentPath?.node?.source?.value;
+}
+
+export const isReactCall = (path: NodePath<t.CallExpression>) => {
   //  TODO: https://zh-hans.reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
   // const isNotImportReact = t.isIdentifier(memberExpression) && jsxAlias.has(memberExpression?.name);
+  const memberExpression = path.node.callee;
+  //@ts-ignore
+  const callName = getImportName(path, path?.node?.callee?.object?.name || '');
   return (
+    callName === 'react' &&
     t.isMemberExpression(memberExpression) &&
     t.isIdentifier(memberExpression.property) &&
     isReactCreateElement(memberExpression.property.name)
