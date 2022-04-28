4cd2c01de3b13a6274ef8803acd8c62b20f0f3c7
fengbo <fengbo.lucky@bytedance.com>
2022-04-28 17:57:37 +0800

    chore: defaultView

--- packages/mona-easybox/src/preset.ts
+++ packages/mona-easybox/src/preset.ts
@@ -7,4 +7,13 @@ export function preset() {
       return '';
     },
   });
+
+  Object.defineProperty(document, 'defaultView', {
+    get: function () {
+      return window.__mona_easy_box.global;
+    },
+    set: function () {
+      return window.__mona_easy_box.global;
+    },
+  });
 }
