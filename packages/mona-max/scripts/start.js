6d11f25e66e3f16a660d715abe10f4de4fa26b08
zhaochenting <zhaochenting@bytedance.com>
2022-05-13 16:26:53 +0800

    feature: use registerTarget

--- packages/mona-max/scripts/start.js
+++ packages/mona-max/scripts/start.js
@@ -1,25 +1,19 @@
 const chalk = require('chalk');
-const webpack = require('webpack')
-const WebpackDevServer = require('webpack-dev-server')
 
-const maxStart = (ctx) => {
-  ctx.registerCommand(
-    'max-start',
-    {
-      description: '店铺装修组件本地开发',
-      usage: 'mona-service max-start',
-    },
-    () => {
+const maxStart = ctx => {
+  ctx.registerTarget('max', tctx => {
+    tctx.overrideStartCommand(() => {
       console.log(chalk.yellow(`🏃 正在启动组件......`));
       const umdConfig = require('../config/webpack.dev')('umd')
       delete require.cache[require.resolve("../config/webpack.dev")]
-      const compiler = webpack(umdConfig);
+      const compiler = tctx.configureWebpack(umdConfig);
       const devServer = new WebpackDevServer({...umdConfig.devServer}, compiler);
       devServer.startCallback(() => {
         console.log(chalk.green('服务启动成功'));
       });
-    }
-  )
-}
+    })
 
-module.exports = maxStart
+  });
+};
+
+module.exports = maxStart;
