35d4ea5b807bdd300cab2d4d7e4ff8963d27cf81
xwchris <zhangxiaowei.chris@bytedance.com>
2022-05-18 16:05:15 +0800

    chore: delete no use deps

--- packages/mona-max/scripts/component.js
+++ packages/mona-max/scripts/component.js
@@ -1,10 +1,8 @@
 const maxComponent = ctx => {
   ctx.registerTarget('max', tctx => {
-
     tctx.configureWebpack(() => {
       if (process.env.NODE_ENV === 'production') {
-        const umdConfig = require('../config/webpack.prod')('umd')
-        return umdConfig;
+        return require('../config/webpack.prod')('umd');
       }
 
       return require('../config/webpack.dev')('umd');
