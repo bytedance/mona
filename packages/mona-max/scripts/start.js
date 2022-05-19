abac5b39bf71c76cc08300ecfb5940282e822e90
xwchris <zhangxiaowei.chris@bytedance.com>
2022-05-19 10:43:45 +0800

    Revert "feature: use registerTarget (#44)"
    This reverts commit 74c83aa6059ce4179636033b4f283e3d83879f78.

--- packages/mona-max/scripts/start.js
+++ packages/mona-max/scripts/start.js
@@ -1,19 +1,25 @@
 const chalk = require('chalk');
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
 
-const maxStart = ctx => {
-  ctx.registerTarget('max', tctx => {
-    tctx.overrideStartCommand(() => {
+const maxStart = (ctx) => {
+  ctx.registerCommand(
+    'max-start',
+    {
+      description: '店铺装修组件本地开发',
+      usage: 'mona-service max-start',
+    },
+    () => {
       console.log(chalk.yellow(`🏃 正在启动组件......`));
       const umdConfig = require('../config/webpack.dev')('umd')
       delete require.cache[require.resolve("../config/webpack.dev")]
-      const compiler = tctx.configureWebpack(umdConfig);
+      const compiler = webpack(umdConfig);
       const devServer = new WebpackDevServer({...umdConfig.devServer}, compiler);
       devServer.startCallback(() => {
         console.log(chalk.green('服务启动成功'));
       });
-    })
+    }
+  )
+}
 
-  });
-};
-
-module.exports = maxStart;
+module.exports = maxStart
