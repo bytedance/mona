4e44201d874636033d87755b40b04f929ee27e0d
fengbo <fengbo.lucky@bytedance.com>
2022-05-23 10:20:48 +0800

    chore: add service plugin

--- packages/mona-service/src/buildInPlugins.ts
+++ packages/mona-service/src/buildInPlugins.ts
@@ -2,15 +2,18 @@ const flatten = (params: any[]) => [].concat(...params);
 export const pathToPlugin = (pathname: string) => require(pathname);
 export const convertPlugins = (plugins: string[]) => flatten(plugins.map(pathToPlugin));
 
-const buildInPlugins = convertPlugins([
-  './commands/build',
-  './commands/start',
-  './commands/compress',
-  './commands/publish',
-  '@bytedance/mona-max',
-  './target/web/index',
-  './target/mini/index',
-  './target/plugin/index',
-]);
+const buildInPlugins = convertPlugins(
+  [
+    process.env.AUTOMATED_TEST === '1' ? '@ecom/mona-dynamic-detection' : '',
+    './commands/build',
+    './commands/start',
+    './commands/compress',
+    './commands/publish',
+    '@bytedance/mona-max',
+    './target/web/index',
+    './target/mini/index',
+    './target/plugin/index',
+  ].filter(r => r),
+);
 
 export default buildInPlugins;
