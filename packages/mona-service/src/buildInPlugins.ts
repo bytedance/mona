eeab7baab0c85579fea48b9d8bc5e554db97eaf4
fengbo <fengbo.lucky@bytedance.com>
2022-05-23 10:21:43 +0800

    chore: plugin filter

--- packages/mona-service/src/buildInPlugins.ts
+++ packages/mona-service/src/buildInPlugins.ts
@@ -1,19 +1,17 @@
 const flatten = (params: any[]) => [].concat(...params);
 export const pathToPlugin = (pathname: string) => require(pathname);
-export const convertPlugins = (plugins: string[]) => flatten(plugins.map(pathToPlugin));
+export const convertPlugins = (plugins: string[]) => flatten(plugins.filter(r => r).map(pathToPlugin));
 
-const buildInPlugins = convertPlugins(
-  [
-    process.env.AUTOMATED_TEST === '1' ? '@ecom/mona-dynamic-detection' : '',
-    './commands/build',
-    './commands/start',
-    './commands/compress',
-    './commands/publish',
-    '@bytedance/mona-max',
-    './target/web/index',
-    './target/mini/index',
-    './target/plugin/index',
-  ].filter(r => r),
-);
+const buildInPlugins = convertPlugins([
+  process.env.AUTOMATED_TEST === '1' ? '@ecom/mona-dynamic-detection' : '',
+  './commands/build',
+  './commands/start',
+  './commands/compress',
+  './commands/publish',
+  '@bytedance/mona-max',
+  './target/web/index',
+  './target/mini/index',
+  './target/plugin/index',
+]);
 
 export default buildInPlugins;
