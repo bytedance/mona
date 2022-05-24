612bd386ca110919b482700fd88dc177f3e445fc
zhaochenting <zhaochenting@bytedance.com>
2022-05-24 23:05:45 +0800

    compatible max-build & max-start

--- packages/mona-max/scripts/component.js
+++ packages/mona-max/scripts/component.js
@@ -1,4 +1,27 @@
+
+import { execSync } from 'child_process';
+
 const maxComponent = ctx => {
+  ctx.registerCommand(
+    'max-build',
+    {
+      description: '店铺装修组件打包',
+      usage: 'mona-service max-build',
+    },
+    () => {
+      execSync('cross-env NODE_ENV=production mona-service build -t max')
+    }
+  )
+  ctx.registerCommand(
+    'max-start',
+    {
+      description: '店铺装修组件本地开发',
+      usage: 'mona-service max-start',
+    },
+    () => {
+      execSync('cross-env NODE_ENV=development mona-service start -t max')
+    }
+  )
   ctx.registerTarget('max', tctx => {
     tctx.configureWebpack(() => {
       if (process.env.NODE_ENV === 'production') {
