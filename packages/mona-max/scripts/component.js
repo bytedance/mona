c9ca3bccf9696f7b656cf926de5343940eb0ce66
zhaochenting <zhaochenting@bytedance.com>
2022-05-24 23:06:34 +0800

    compatible max-build & max-start

--- packages/mona-max/scripts/component.js
+++ packages/mona-max/scripts/component.js
@@ -1,27 +1,7 @@
 
-import { execSync } from 'child_process';
+const child_process = require('child_process');
 
 const maxComponent = ctx => {
-  ctx.registerCommand(
-    'max-build',
-    {
-      description: '店铺装修组件打包',
-      usage: 'mona-service max-build',
-    },
-    () => {
-      execSync('cross-env NODE_ENV=production mona-service build -t max')
-    }
-  )
-  ctx.registerCommand(
-    'max-start',
-    {
-      description: '店铺装修组件本地开发',
-      usage: 'mona-service max-start',
-    },
-    () => {
-      execSync('cross-env NODE_ENV=development mona-service start -t max')
-    }
-  )
   ctx.registerTarget('max', tctx => {
     tctx.configureWebpack(() => {
       if (process.env.NODE_ENV === 'production') {
@@ -31,6 +11,26 @@ const maxComponent = ctx => {
       return require('../config/webpack.dev')('umd');
     })
   });
+  ctx.registerCommand(
+    'max-build',
+    {
+      description: '店铺装修组件打包',
+      usage: 'mona-service max-build',
+    },
+    () => {
+      child_process.execSync('cross-env NODE_ENV=production mona-service build -t max')
+    }
+  )
+  ctx.registerCommand(
+    'max-start',
+    {
+      description: '店铺装修组件本地开发',
+      usage: 'mona-service max-start',
+    },
+    () => {
+      child_process.execSync('cross-env NODE_ENV=development mona-service start -t max')
+    }
+  )
 };
 
 module.exports = maxComponent;
