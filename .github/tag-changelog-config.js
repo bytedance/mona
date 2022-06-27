203fbbdd574d899edfb36568eb60002811f4ef1a
fengbo.lucky <fengbo.lucky@bytedance.com>
2022-06-27 10:07:37 +0800

    chore: changelog

--- .github/tag-changelog-config.js
+++ .github/tag-changelog-config.js
@@ -15,18 +15,18 @@ module.exports = {
 
   excludeTypes: ['other'],
 
-  renderTypeSection: function (label, commits) {
-    let text = `\n## ${label}\n`;
+  // renderTypeSection: function (label, commits) {
+  //   let text = `\n## ${label}\n`;
 
-    commits.forEach(commit => {
-      text += `- ${commit.subject}\n`;
-    });
+  //   commits.forEach(commit => {
+  //     text += `- ${commit.subject}\n`;
+  //   });
 
-    return text;
-  },
+  //   return text;
+  // },
 
-  renderChangelog: function (release, changes) {
-    const now = new Date();
-    return `# ${release} - ${now.toISOString().substr(0, 10)}\n` + changes + '\n\n';
-  },
+  // renderChangelog: function (release, changes) {
+  //   const now = new Date();
+  //   return `# ${release} - ${now.toISOString().substr(0, 10)}\n` + changes + '\n\n';
+  // },
 };
