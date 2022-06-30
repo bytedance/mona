7f9068429d309a1ecb316e63825e799e38336bda
zhaochenting <zhaochenting@bytedance.com>
2022-06-30 20:34:36 +0800

    feat: max template cli

--- packages/mona-max/scripts/component.js
+++ packages/mona-max/scripts/component.js
@@ -1,3 +1,8 @@
+
+const child_process = require('child_process');
+const chalk = require('chalk');
+const path = require('path');
+
 const maxComponent = ctx => {
   ctx.registerTarget('max', tctx => {
     tctx.configureWebpack(() => {
@@ -8,6 +13,54 @@ const maxComponent = ctx => {
       return require('../config/webpack.dev')('umd');
     })
   });
+  ctx.registerCommand(
+    'max-template-start',
+    {
+      description: '店铺装修模版start',
+      usage: 'mona-service max-template-start',
+    },
+    () => {
+      const configPath = path.resolve(__dirname, '../utils/templateStart.js');
+      child_process.execSync(`node ${configPath}`, function (error, stdout, stderr) {
+        if (error) {
+          console.log(error.stack);
+        }
+        console.log(stdout);
+      });
+    }
+  )
+  ctx.registerCommand(
+    'max-build',
+    {
+      description: '店铺装修组件打包',
+      usage: 'mona-service max-build',
+    },
+    () => {
+      console.log(chalk.bold.red("请更新build命令为 'mona-service build -t max'"))
+      child_process.execSync('mona-service build -t max', function (error, stdout, stderr) {
+        if (error) {
+          console.log(error.stack);
+        }
+        console.log(stdout);
+      })
+    }
+  )
+  ctx.registerCommand(
+    'max-start',
+    {
+      description: '店铺装修组件本地开发',
+      usage: 'mona-service max-start',
+    },
+    () => {
+      console.log(chalk.bold.red("请更新start命令为 'mona-service start -t max'"))
+      child_process.execSync('mona-service start -t max', function (error, stdout, stderr) {
+        if (error) {
+          console.log(error.stack);
+        }
+        console.log(stdout);
+      })
+    }
+  )
 };
 
 module.exports = maxComponent;
---
+++ packages/mona-max/utils/getTmpData.js
@@ -0,0 +1,98 @@
+const { C_TYPE_NEW_ISV } = require('./constants.js');
+
+const isJSONStringifyLegal = v => {
+  try {
+    JSON.stringify(v);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+function transformSchemaValue(value) {
+  if (!value) return '';
+  const keysOfValue = Object.keys(value);
+  if (!Array.isArray(keysOfValue)) return '';
+
+  let res = [];
+
+  keysOfValue.forEach(key => {
+    const v = value[key] ? JSON.parse(value[key]) : null;
+    const { type, value: scheme_value } = v || {};
+
+    let realValue = scheme_value;
+    if (Array.isArray(scheme_value) && type === C_TYPE_NEW_ISV.listContainer.value) {
+      realValue = scheme_value.map(i => {
+        if (i && i.type === C_TYPE_NEW_ISV.objectContainer.value && i.value) {
+          const res = {
+            c_type: i.type,
+            name: i.name,
+            reject_info: null,
+            scheme_value: {},
+          };
+          Object.keys(i.value).forEach(j => {
+            const vForObj = i.value[j];
+            res.scheme_value[j] = {
+              c_type: vForObj?.type,
+              scheme_value: vForObj && vForObj.value ? JSON.stringify(vForObj.value) : '',
+              name: j,
+              reject_info: null,
+            };
+          });
+          res.scheme_value = JSON.stringify(res.scheme_value);
+          return res;
+        }
+
+        return {
+          c_type: i?.type,
+          name: i?.name,
+          reject_info: null,
+          scheme_value: i && i.value ? JSON.stringify(i.value) : '',
+        };
+      });
+    }
+
+    if (type === C_TYPE_NEW_ISV.objectContainer.value) {
+      const res = {};
+      Object.keys(scheme_value).forEach(j => {
+        const vForObj = scheme_value[j];
+        res[j] = {
+          c_type: vForObj?.type,
+          scheme_value: vForObj?.value ? JSON.stringify(vForObj.value) : '',
+          name: j,
+          reject_info: null,
+        };
+      });
+      realValue = res;
+    }
+
+    res.push({
+      c_type: type,
+      scheme_value: isJSONStringifyLegal(realValue) ? JSON.stringify(realValue) : '',
+      name: key,
+      reject_info: null,
+    });
+  });
+
+  return res;
+};
+
+module.exports = function finalData(value) {
+  if (!value) return '';
+  const keysOfValue = Object.keys(value);
+  if (!Array.isArray(keysOfValue)) return '';
+
+  let res = [];
+
+  value.forEach(key => {
+    const item = {
+      componentId: key.component_id,
+      componentVersion: key.version,
+    }
+    const componentValue = transformSchemaValue(key.data.value)
+    item.value = JSON.stringify(componentValue)
+    res.push(item)
+  })
+
+  return JSON.stringify(res)
+}
