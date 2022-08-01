9dcd43d976a8cc39c3bbac8e154f0b6767c64180
xwchris <zhangxiaowei.chris@bytedance.com>
2022-08-01 17:53:10 +0800

    chore: add preview for light

--- packages/mona-service/src/commands/preview/index.ts
+++ packages/mona-service/src/commands/preview/index.ts
@@ -1,19 +1,18 @@
 import { IPlugin } from "@/Service";
-// import path from 'path';
-import { generateQrcodeFactory, createTestVersionFactory, pipe, buildMaxComponent, processMaxComponentData, processMaxTemplateData, printQrcode } from "./utils";
+import { generateQrcodeFactory, createTestVersionFactory, pipe, buildMaxComponent, processMaxComponentData, processMaxTemplateData, printQrcode, getPlatform, getUrl, openUrlWithBrowser } from "./utils";
 import { generateRequestFromOpen, requestBeforeCheck } from "../common";
+import chalk from "chalk";
 
 const preview: IPlugin = (ctx) => {
   ctx.registerCommand('preview', {
     options: [
       { name: 'help', description: '输出帮助信息', alias: 'h' },
       { name: 'target', description: '目标端', alias: 't' },
-      { name: 'watch', description: '是否监听文件更改', alias: 'w' },
+      // { name: 'watch', description: '是否监听文件更改', alias: 'w' },
+      { name: 'platform', description: '平台类型（当target为light即轻应用时，有效值为compass，不填默认compass）', alias: 'p' },
     ]
   }, async (args) => {
     // output dir
-    // const dist = ctx.configHelper.projectConfig.output || 'dist';
-    // const outputDir = path.join(ctx.configHelper.cwd, dist);
 
     // assert
     const { user } = await requestBeforeCheck(ctx, args);
@@ -21,16 +20,21 @@ const preview: IPlugin = (ctx) => {
     const request = generateRequestFromOpen(args, user.cookie)
 
     // common steps for all target: compress => upload
-    const commonProcess = [createTestVersionFactory(request), generateQrcodeFactory(request), printQrcode];
+    const maxProcess = [createTestVersionFactory(request), generateQrcodeFactory(request), printQrcode];
 
-    if (args.target === 'max') {
-      await pipe(buildMaxComponent, processMaxComponentData, ...commonProcess)(ctx)
-    } else if (args.target === 'max-template') {
-      await pipe(processMaxTemplateData, ...commonProcess)(ctx)
-    } else {
-      console.error(`${args.target}端目前不支持preview命令，敬请期待`)
+    switch(args.target) {
+      case 'max':
+        await pipe(buildMaxComponent, processMaxComponentData, ...maxProcess)(ctx);
+        break;
+      case 'max-template':
+        await pipe(processMaxTemplateData, ...maxProcess)(ctx);
+        break;
+      case 'light':
+        pipe(getPlatform, getUrl, openUrlWithBrowser)({ ctx, args });
+        break;
+      default:
+        console.log(chalk.red(`${args.target}端目前暂不支持preview命令，敬请期待`))
     }
-
     // watch(outputDir, {
     //   open: !!args.watch
     // }, () => {
