4e120b4b32525264db84bb6eb04b32edd7e84ade
bytedance <baihaihui@bytedance.com>
2022-08-03 20:38:32 +0800

    chore: 16 to 50

--- packages/mona-max/config/webpack.base.js
+++ packages/mona-max/config/webpack.base.js
@@ -20,7 +20,7 @@ const generateBaseConfig = options => {
     postcssPlugins = [
       PostcssPluginRpxToVw,
       Pxtorem({
-        rootValue: 16, //结果为：设计稿元素尺寸/16，比如元素宽320px,最终页面会换算成 20rem
+        rootValue: 50, //结果为：设计稿元素尺寸/16，比如元素宽320px,最终页面会换算成 20rem
         propList: ['*'],
         exclude: /node_modules/i, //这里表示不处理node_modules文件夹下的内容
       }),
