baf64b49da5e975b7eeb887fe0eaefa78405f5ee
fengbo.lucky <fengbo.lucky@bytedance.com>
2022-08-25 15:49:24 +0800

    fix: miniLifeCycle

--- packages/mona-clients/mona-client-mini/src/index.ts
+++ packages/mona-clients/mona-client-mini/src/index.ts
@@ -4,3 +4,5 @@ export { default as createPage } from './createPage';
 export { default as createMiniComponent } from './createMiniComponent';
 export * from './apis';
 export { useMiniEffect } from './miniEffect';
+
+export { usePageEvent, useAppEvent, AppLifecycle, PageLifecycle } from '@bytedance/mona';
