c68eb5c0c65311479a1a846571964a94b49f7988
fengbo.lucky <fengbo.lucky@bytedance.com>
2022-08-26 11:18:29 +0800

    chore: api

--- packages/mona-clients/mona-client-mini/src/index.ts
+++ packages/mona-clients/mona-client-mini/src/index.ts
@@ -2,7 +2,9 @@ export * from './components';
 export { default as createApp } from './createApp';
 export { default as createPage } from './createPage';
 export { default as createMiniComponent } from './createMiniComponent';
-export * from './apis';
+export * from './apis/api';
+export { Mona } from './apis';
+
 export { useMiniEffect } from './miniEffect';
 
 export { usePageEvent, useAppEvent, AppLifecycle, PageLifecycle } from '@bytedance/mona';
