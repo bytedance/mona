fe8548f1caea703dc1d1688986f54090d4cacdeb
fengbo.lucky <fengbo.lucky@bytedance.com>
2022-08-29 16:30:17 +0800

    chore: .git

--- packages/mona-service/src/commands/compress/utils.ts
+++ packages/mona-service/src/commands/compress/utils.ts
@@ -33,7 +33,7 @@ export async function compressDir(inputPath: string, ignoreList: string[] = []) 
   const spinner = ora('打包中...').start();
   ensureDirExist(TEMP_DIR);
   const outputPath = path.join(process.cwd(), TEMP_DIR, `publish${Date.now()}.zip`);
-  await compressToZip(inputPath, outputPath, [...ignoreList, 'node_modules', '.git', TEMP_DIR]);
+  await compressToZip(inputPath, outputPath, [...ignoreList, 'node_modules', '^.git$', TEMP_DIR]);
   spinner.succeed(chalk.green('打包成功'));
   return outputPath;
 }
