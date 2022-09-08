eb14bddcae86247e40d3f524b0be610d374edfa1
xwchris <zhangxiaowei.chris@bytedance.com>
2022-09-08 15:37:58 +0800

    chore: fix qrcode bug in windows

--- packages/mona-service/src/commands/preview/utils.ts
+++ packages/mona-service/src/commands/preview/utils.ts
@@ -1,18 +1,19 @@
-
 import QRCode from 'qrcode';
 import chokidar from 'chokidar';
 import open from 'open';
 import PluginContext from '@/PluginContext';
 import { execSync } from 'child_process';
-import { compressDistDir } from "../compress/utils";
+import { compressDistDir } from '../compress/utils';
 import { createUploadForm, FileType } from '../common';
 import { AxiosRequestConfig } from 'axios';
 import path from 'path';
 import fs from 'fs';
+import os from 'os';
 import chalk from 'chalk';
 import { DEFAULT_PORT } from '@/target/constants';
 
-type Request<T = any> = (path: string, options?: AxiosRequestConfig<any>) => Promise<T>
+const isWin = os.platform() === 'win32';
+type Request<T = any> = (path: string, options?: AxiosRequestConfig<any>) => Promise<T>;
 
 interface GetDynamicTestUrlResp {
   secShopId: string;
@@ -27,21 +28,22 @@ interface CreateTestAppVersionResp {
 }
 
 // pipe func
-export const pipe = (...funcs: Function[]) => (input?: any) => funcs.reduce((prev, cur) => (i: any) => {
-  const res = prev(i);
-  const isPromise = typeof res?.then === 'function';
-  if (isPromise) {
-    return res.then((r: any) => cur(r));
-  } else {
-    return cur(res);
-  }
-})(input);
+export const pipe = (...funcs: Function[]) => (input?: any) =>
+  funcs.reduce((prev, cur) => (i: any) => {
+    const res = prev(i);
+    const isPromise = typeof res?.then === 'function';
+    if (isPromise) {
+      return res.then((r: any) => cur(r));
+    } else {
+      return cur(res);
+    }
+  })(input);
 
 // watch dir
 export function watch(dir: string, options: { open: boolean }, callback: Function) {
   if (options.open) {
-    console.log(`watching ${dir}`)
-    let timer: NodeJS.Timeout | null = null
+    console.log(`watching ${dir}`);
+    let timer: NodeJS.Timeout | null = null;
     chokidar.watch(dir).on('all', () => {
       // debounce
       if (timer) {
@@ -51,23 +53,25 @@ export function watch(dir: string, options: { open: boolean }, callback: Functio
         console.clear();
         console.log('reprocess');
         callback();
-      }, 500)
-    })
+      }, 500);
+    });
   } else {
-    callback()
+    callback();
   }
 }
 
-export const createTestVersionFactory = (request: Request<CreateTestAppVersionResp>) => async (params: Record<string, string | FileType>) => {
-  const { form, requestOptions } = await createUploadForm(params)
+export const createTestVersionFactory = (request: Request<CreateTestAppVersionResp>) => async (
+  params: Record<string, string | FileType>,
+) => {
+  const { form, requestOptions } = await createUploadForm(params);
   const res = await request('/captain/app/version/test/create', {
     method: 'POST',
     data: form,
     ...requestOptions,
-  })
+  });
 
   return { appId: params.appId, version: res.version };
-}
+};
 
 function formatNumberToTwoDigit(number: number) {
   return `0${number}`.slice(-2);
@@ -82,38 +86,41 @@ export function getFormatedExpireTime(seconds: number) {
   const minute = formatNumberToTwoDigit(date.getMinutes());
   const second = formatNumberToTwoDigit(date.getSeconds());
 
-  return `${year}/${month}/${day} ${hour}:${minute}:${second}`
+  return `${year}/${month}/${day} ${hour}:${minute}:${second}`;
 }
 
-export function printQrcode(params: { qrcode: string, expireTime: number }) {
+export function printQrcode(params: { qrcode: string; expireTime: number }) {
   console.log(params.qrcode);
-  console.log(chalk.yellow(`二维码 ${getFormatedExpireTime(params.expireTime)} 到期，请尽快使用抖音进行扫码预览！`))
+  console.log(chalk.yellow(`二维码 ${getFormatedExpireTime(params.expireTime)} 到期，请尽快使用抖音进行扫码预览！`));
 }
 
+export const generateQrcodeFactory = (request: Request<GetDynamicTestUrlResp>) => async (params: {
+  appId: string;
+  version: string;
+}) => {
+  const res = await request('/captain/app/version/getDynamicTestUrl', {
+    method: 'GET',
+    params: {
+      ...params,
+      previewScene: 2,
+    },
+  });
 
-export const generateQrcodeFactory = (request: Request<GetDynamicTestUrlResp>) => async (params: { appId: string; version: string }) => {
-    const res = await request('/captain/app/version/getDynamicTestUrl', {
-      method: 'GET',
-      params: {
-        ...params,
-        previewScene: 2
+  const preViewCodeUrl = `aweme://goods/store?sec_shop_id=${res?.secShopId}&token=${res?.token}&tmp_id=${res?.tmpId}&enter_from=scan&entrance_location=scan&pass_through_api=%7B%22isJump%22%3A1%7D`;
+  const qrcode = await new Promise((resolve, reject) => {
+    // @ts-ignore
+    // qrcode render failed in windows terminal when options with small: true
+    QRCode.toString(preViewCodeUrl, { type: 'terminal', small: !isWin }, (err, url) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(url);
       }
-    })
+    });
+  });
 
-    const preViewCodeUrl = `aweme://goods/store?sec_shop_id=${res?.secShopId}&token=${res?.token}&tmp_id=${res?.tmpId}&enter_from=scan&entrance_location=scan&pass_through_api=%7B%22isJump%22%3A1%7D`;
-    const qrcode = await new Promise((resolve, reject) => {
-      // @ts-ignore
-      QRCode.toString(preViewCodeUrl, { type: 'terminal', small: true }, (err, url) => {
-        if (err) {
-          reject(err)
-        } else {
-          resolve(url)
-        }
-      })
-    })
-
-    return { qrcode, expireTime: res.expireTime }
-}
+  return { qrcode, expireTime: res.expireTime };
+};
 
 export function buildMaxComponent(ctx: PluginContext) {
   console.log('build');
@@ -127,7 +134,7 @@ export async function processMaxComponentData(ctx: PluginContext) {
   const { appId = '', output } = helper.projectConfig;
 
   // compress
-  const filePath = await compressDistDir(output)
+  const filePath = await compressDistDir(output);
 
   // read value from preview.json
   const componentValuePath = path.join(helper.cwd, 'src/preview.json');
@@ -139,7 +146,7 @@ export async function processMaxComponentData(ctx: PluginContext) {
       filePath,
     },
     componentAppDefaultValue,
-  }
+  };
 }
 
 // process max template data
@@ -153,28 +160,30 @@ export async function processMaxTemplateData(ctx: PluginContext) {
 
   return {
     appId,
-    templateAppDefaultValue
-  }
+    templateAppDefaultValue,
+  };
 }
 
-export function getPlatform(params: { ctx: PluginContext, args: Record<string, string> }) {
+export function getPlatform(params: { ctx: PluginContext; args: Record<string, string> }) {
   const { ctx, args } = params;
   // judge env
   const result = (args.domain || '').match(/^[^\.]+(\.[^\.]+\.[^\.]+)/);
   const isTest = result && result[1] !== '.jinritemai.com';
-  const platformUrlMap: Record<string, string> = isTest ? {
-    compass: `https://ecom-compass-boe${result[1]}/shop/search-analysis`
-  } : {
-    compass: 'https://compass.jinritemai.com/shop/search-analysis',
-  }
+  const platformUrlMap: Record<string, string> = isTest
+    ? {
+        compass: `https://ecom-compass-boe${result[1]}/shop/search-analysis`,
+      }
+    : {
+        compass: 'https://compass.jinritemai.com/shop/search-analysis',
+      };
   return {
     platform: Object.keys(platformUrlMap).indexOf(args.platform) !== -1 ? args.platform : 'compass',
     ctx,
-    platformUrlMap
+    platformUrlMap,
   };
 }
 
-export function getUrl(params: { ctx: PluginContext, platform: string, platformUrlMap: Record<string, string> }) {
+export function getUrl(params: { ctx: PluginContext; platform: string; platformUrlMap: Record<string, string> }) {
   const { ctx, platform, platformUrlMap } = params;
   const url = platformUrlMap[platform];
 
