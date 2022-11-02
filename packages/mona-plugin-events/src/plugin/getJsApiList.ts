d6da39b503a6792b95e56089df1c12a415339d48
kenny <xiaokun.kenny@bytedance.com>
2022-11-02 15:28:13 +0800

    chore: max add ppe request

--- packages/mona-plugin-events/src/plugin/getJsApiList.ts
+++ packages/mona-plugin-events/src/plugin/getJsApiList.ts
@@ -2,11 +2,11 @@ import { JsApiListResponse } from '../type';
 import axios from 'axios';
 
 //获取jsapi列表
-export const getJsApiList = (boeUrl?: string) => {
+export const getJsApiList = (env: 'prod' | 'boe' | 'ppe' = 'prod', url?: string) => {
   const getJsApiListUrl = 'https://ecom-openapi.ecombdapi.com/open/jsapi';
-  if (boeUrl) {
+  if (env === 'boe' && url) {
     return axios({
-      url: boeUrl,
+      url: url,
       data: {
         biz_domain: '测试',
       },
@@ -17,9 +17,22 @@ export const getJsApiList = (boeUrl?: string) => {
         'Content-Type': 'application/json',
       },
     }).then(res => res?.data);
+  } else if (env === 'ppe') {
+    return axios({
+      url: getJsApiListUrl,
+      data: {
+        biz_domain: '测试',
+      },
+      method: 'post',
+      headers: {
+        'x-use-ppe': '1',
+        'x-tt-env': 'ppe_6657203',
+        'Content-Type': 'application/json',
+      },
+    }).then(res => res?.data);
   } else {
     return axios({
-      url: boeUrl,
+      url: getJsApiListUrl,
       data: {
         biz_domain: 'shop_decorate',
       },
