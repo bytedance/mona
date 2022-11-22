0b8b70bf89593db0477f54d204a88d0324c1ffc5
xwchris <zhangxiaowei.chris@bytedance.com>
2022-11-22 15:41:43 +0800

    chore: transform event name

--- packages/mona-service/src/plugins/babel/TransformJsxLabel.ts
+++ packages/mona-service/src/plugins/babel/TransformJsxLabel.ts
@@ -1,6 +1,21 @@
 import { NodePath } from '@babel/traverse';
 import * as t from '@babel/types';
 
+const eventMap: { [key: string]: string } = {
+  'tap': 'click'
+}
+
+function transformEventName(name: string) {
+  const reg = /^bind/;
+  if (!reg.test(name)) {
+    return name;
+  }
+
+  const value = name.replace(reg, '');
+  const newValue = eventMap[value] || value;
+  return `on${newValue.replace(newValue[0], newValue[0].toUpperCase())}`;
+}
+
 export default function TransformJsxLabelPlugin() {
   return {
     visitor: {
@@ -9,6 +24,8 @@ export default function TransformJsxLabelPlugin() {
           path.node.name = 'div';
         } else if (path.node.name === 'text') {
           path.node.name = 'span';
+        } else if (path.node.name.startsWith('bind')) {
+          path.node.name = transformEventName(path.node.name)
         }
       },
     },
