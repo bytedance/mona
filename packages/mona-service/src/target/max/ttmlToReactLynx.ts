25426a5b05aea3c78abb3fd6347b7b64f53a6004
xwchris <zhangxiaowei.chris@bytedance.com>
2022-12-22 11:29:30 +0800

    chore: fix slash for entry

--- packages/mona-service/src/target/max/ttmlToReactLynx.ts
+++ packages/mona-service/src/target/max/ttmlToReactLynx.ts
@@ -400,7 +400,7 @@ export const ttmlToReactLynx = (tempTTMLDir: string, configHelper: ConfigHelper)
       deleteFile(path.join(sourceDir, `${filename}.ttss`))
     }
   })
-  const entry = entryInfo.entry.replace(tempTTMLDir, '').replace(/\.js$/, '');
-  return /^\/./.test(entry) ? `.${entry}` : `./${entry}`;
+  const entry = `./${path.relative(tempTTMLDir, entryInfo.entry)}`.replace(/\.js$/, '').replace('\\', '/');
+  return entry;
 };
 
