c80b944ea3bd03a7db5c43c4e3c4fef1d5c0a70f
liwentao.8 <liwentao.8@bytedance.com>
2023-03-29 11:49:36 +0800

    feat: 自动化测试cli

--- packages/mona-manager-plugins/mona-cli-commands/src/index.ts
+++ packages/mona-manager-plugins/mona-cli-commands/src/index.ts
@@ -2,4 +2,5 @@ const init = require('./init');
 const login = require('./login');
 const logout = require('./logout');
 const update = require('./update');
-module.exports = [init, login, logout, update()];
+const autoTest = require('./auto-test');
+module.exports = [init, login, logout, update(), autoTest];
