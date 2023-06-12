b8971a1b8557033d221a6198aef45b047118b068
fengbo.lucky <fengbo.lucky@bytedance.com>
2023-06-12 15:58:05 +0800

    chore: version

--- packages/mona-service/src/utils/updateOldVersion.ts
+++ packages/mona-service/src/utils/updateOldVersion.ts
@@ -13,15 +13,12 @@ const checkVersionPlugin: IPlugin = ctx => {
     if (target !== 'light') {
       return;
     }
-    const { cwd } = ctx.configHelper;
-    const staticDir = path.join(cwd, './package.json');
-    const pkg = require(staticDir);
-    let currRuntimeVersion = pkg['dependencies']['@bytedance/mona-runtime'];
-    if (currRuntimeVersion === 'latest') {
-      currRuntimeVersion = require(require
-        .resolve('@bytedance/mona-runtime')
-        .replace('dist/index.js', 'package.json')).version;
-    }
+    // const { cwd } = ctx.configHelper;
+    // const staticDir = path.join(cwd, './package.json');
+    // const pkg = require(staticDir);
+    const currRuntimeVersion = require(require
+      .resolve('@bytedance/mona-runtime')
+      .replace('dist/index.js', 'package.json')).version;
 
     if (currRuntimeVersion) {
       const res = semver.compare('0.3.12', currRuntimeVersion);
